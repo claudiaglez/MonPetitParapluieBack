0bbde3f18607a945396c2f7b175ced0eacfa9ee7
Claudia González <claudia.glezg@gmail.com>
2024-03-14 15:23:52 +0100

    add index and show to ArticleController

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use Illuminate\Http\Request;
+use Exception;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        try {
+            $articles = Article::all();
+            return response()->json(['status' => 200, 'data' => $articles]);
+        } catch (Exception $e) {
+            return response()->json(['status' => 204, 'message' => $e->getMessage()], 204);
+        }
+    }
+
+    public function show($id)
+    {
+        $article = Article::find($id);
+
+        if ($article) {
+            return response()->json(['article' => $article], 200);
+        } else {
+            return response()->json(['error' => 'Artículo no encontrado'], 404);
+        }
+    }
+
+    
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     protected $fillable = [
         'category',
     ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
